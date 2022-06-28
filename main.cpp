31de3f03762075828693be8a0c631456ec148b78
Jakub Wodnicki <jakub.jan.wodnicki@gmail.com>
2022-06-28 20:10:25 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,25 @@
 
 #include <iostream>
+#include <string.h>
+#include <fstream>
 using namespace std;
 
 int main (void)
 {
-      srand (time (0));
+
+
+      srand(time (0));
       int losowa = rand () % 100 + 1;
       int zgadywana = 0;
+	  int ruch = 0;
+	  string imie;
 
       cout << "Wylosowalem liczbe od 1 do 100, postaraj sie ja odgadnac!" <<endl;
 
       while (zgadywana != losowa)
 	{
 	  cout << "Podaj liczbe: ";
+	  ruch++;
 	  cin >> zgadywana;
 
 	  if (zgadywana > losowa)
@@ -25,7 +32,15 @@ int main (void)
 	      cout <<"\nNiestety, sprobuj ponownie. Twoja liczba jest mniejsza niz wylosowana."<<endl;
 	    }
 	}
-      cout << "\nBrawo! Udalo Ci sie odgadnac liczbe!";
-
+	
+      cout << "\nBrawo! Udalo Ci sie odgadnac liczbe!"<<endl;
+	  cout << "\nIlosc podejsc: " << ruch;
+	  cout << "\nPodaj imie: ";
+	  cin >> imie;
+	
+	ofstream o;
+	o.open("wyniki.txt");
+	o << imie << ruch;
+	o.close();
 }
 
