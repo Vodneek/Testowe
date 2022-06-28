15e88cb0d38b7cabbf611321fc3d4bfadb6e9ef1
Jakub Wodnicki <jakub.jan.wodnicki@gmail.com>
2022-06-28 20:56:40 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,46 +1,61 @@
-
 #include <iostream>
-#include <string.h>
 #include <fstream>
+#include <string>
 using namespace std;
 
-int main (void)
+int main(void)
 {
+	srand(time(0));
+	int losowa = rand() % 100 + 1;
+	int zgadywana = 0;
+	int ruch = 0;
+	string imie;
+	int count = 0;
 
+	cout << losowa;
+	cout << "Wylosowalem liczbe od 1 do 100, postaraj sie ja odgadnac!" << endl;
 
-      srand(time (0));
-      int losowa = rand () % 100 + 1;
-      int zgadywana = 0;
-	  int ruch = 0;
-	  string imie;
-
-      cout << "Wylosowalem liczbe od 1 do 100, postaraj sie ja odgadnac!" <<endl;
-
-      while (zgadywana != losowa)
+	while (zgadywana != losowa)
 	{
-	  cout << "Podaj liczbe: ";
-	  ruch++;
-	  cin >> zgadywana;
+		cout << "Podaj liczbe: ";
+		ruch++;
+		cin >> zgadywana;
 
-	  if (zgadywana > losowa)
-	    {
-	      cout <<"\nNiestety, sprobuj ponownie. Twoja liczba jest wieksza niz wylosowana."<<endl;
-	    }
+		if (zgadywana > losowa)
+		{
+			cout << "\nNiestety, sprobuj ponownie. Twoja liczba jest wieksza niz wylosowana." << endl;
+		}
 
-	  else if (zgadywana < losowa)
-	    {
-	      cout <<"\nNiestety, sprobuj ponownie. Twoja liczba jest mniejsza niz wylosowana."<<endl;
-	    }
+		else if (zgadywana < losowa)
+		{
+			cout << "\nNiestety, sprobuj ponownie. Twoja liczba jest mniejsza niz wylosowana." << endl;
+		}
 	}
-	
-      cout << "\nBrawo! Udalo Ci sie odgadnac liczbe!"<<endl;
-	  cout << "\nIlosc podejsc: " << ruch;
-	  cout << "\nPodaj imie: ";
-	  cin >> imie;
-	
+
+	cout << "\nBrawo! Udalo Ci sie odgadnac liczbe!" << endl;
+	cout << "\nIlosc podejsc: " << ruch;
+	cout << "\nPodaj imie: ";
+	cin >> imie;
+
+
 	ofstream o;
-	o.open("wyniki.txt");
-	o << imie << ruch;
-	o.close();
+	ifstream i;
+
+	o.open("wyniki.txt", std::ios_base::app);
+	o << imie << "     " << ruch << endl;
+	o.close();	
+
+	cout << "\nWyniki\n";
+	cout << "____________________\n";
+	cout << "Imie     Ilosc ruchow\n";
+	i.open("wyniki.txt", ios::in);
+	if (i.is_open()) {  
+		string tp;
+		while (getline(i, tp)) {
+			count++;
+			cout <<count<<"."<< tp << "\n";
+		}
+		i.close();
+	}
 }
 
